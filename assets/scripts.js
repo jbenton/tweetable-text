2929da95149cf5cb9b4a4c5621d4a58096d5f07f
Justin Chick <hey@justinchick.com>
2014-01-21 20:41:20 -0600

    create css/js assets

---
+++ assets/scripts.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function(){
+  $(".tweetable").hover(
+      function(){
+            if ($(this).data('vis') != true) {
+                    $(this).data('vis', true);
+                    $(this).find('.sharebuttons').fadeIn(200);
+            }
+      },
+      function(){
+            if ($(this).data('vis') === true) {
+                    $(this).find('.sharebuttons').clearQueue().delay(0).fadeOut(200);
+                    $(this).data('vis', false);
+                    $(this).data('leftSet', false);
+            }
+      });
+});	
